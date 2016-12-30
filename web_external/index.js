51dba42b75e2683c9cb2e72a0c0b3a948d903847
Roni Choudhury <roni.choudhury@kitware.com>
2016-12-30 17:36:30 -0500

    Add starting screen overlay

--- web_external/index.js
+++ web_external/index.js
@@ -5,9 +5,16 @@ console.log(store.getState());
 
 import html from './index.jade';
 import { initialize as initHeader } from './view/layout/header';
+import { initialize as initStartingScreen } from './view/overlay/StartingScreen';
+
+import './view/overlay/index.styl';
+import './index.styl';
 
 // Instantiate the main application template.
 select('#app').html(html);
 
 // Instantiate the header.
 initHeader(select('#header'));
+
+// Instantiate the starting screen.
+initStartingScreen(select('#overlay'));
---
+++ web_external/view/overlay/StartingScreen/index.js
@@ -0,0 +1,113 @@
+import html from './index.jade';
+import './index.styl';
+
+import folderIcon from '../../../image/light/folder.svg';
+import fileIcon from '../../../image/light/file.svg';
+import scatterplotIcon from '../../../image/light/scatterplot.svg';
+import datasetIcon from '../../../image/light/dataset.svg';
+import reslabBanner from '../../../image/Resonant_Lab_cropped.svg';
+import reslabLogo from '../../../image/Resonant_Lab_Mark.svg';
+import resonantLogo from '../../../image/Resonant_Mark.svg';
+import kitwareLogo from '../../../image/Kitware_Mark.svg';
+import githubLogo from '../../../image/Github_Mark.svg';
+import contactIcon from '../../../image/contact.svg';
+import closeIcon from '../../../image/close.svg';
+
+const loggedIn = false;
+
+const initialize = (sel) => {
+  sel.html(html({
+    folderIcon,
+    fileIcon,
+    scatterplotIcon,
+    datasetIcon,
+    reslabBanner,
+    reslabLogo,
+    resonantLogo,
+    kitwareLogo,
+    githubLogo,
+    contactIcon,
+    closeIcon,
+    loggedIn
+	}));
+}
+
+const render = () => {
+
+};
+
+export {
+  initialize,
+  render
+};
+
+/*
+let StartingScreen = Backbone.View.extend({
+  initialize: function () {
+    this.listenTo(window.mainPage.currentUser, 'rl:login', this.render);
+    this.listenTo(window.mainPage.currentUser, 'rl:logout', this.render);
+    this.listenTo(window.mainPage, 'rl:changeProject', this.render);
+  },
+  render: function () {
+    if (!this.addedTemplate) {
+      this.$el.html(myTemplate);
+
+      this.$el.find('#startWithVisButton').on('click', () => {
+        // TODO: load the stock library project
+        // from the vis library instead of the vis itthis
+        window.mainPage.overlay.render('VisualizationLibrary');
+      });
+
+      this.$el.find('#startWithDataButton').on('click', () => {
+        // TODO: load the stock library project
+        // from the dataset library instead of the dataset itthis
+        window.mainPage.overlay.render('DatasetLibrary');
+      });
+
+      this.$el.find('#emptyProjectButton').on('click', () => {
+        window.mainPage.newProject()
+          .then(() => {
+            window.mainPage.widgetPanels.closeWidgets();
+            window.mainPage.overlay.closeOverlay();
+          });
+      });
+
+      this.$el.find('#openProjectButton').on('click', () => {
+        window.mainPage.overlay.render('ProjectLibrary');
+      });
+
+      this.$el.find('a#loginLink').on('click', () => {
+        window.mainPage.overlay.render('LoginView');
+      });
+
+      this.$el.find('a#registerLink').on('click', () => {
+        window.mainPage.overlay.render('RegisterView');
+      });
+
+      this.$el.find('a#logoutLink').on('click', () => {
+        window.mainPage.currentUser.authenticate(false);
+      });
+
+      this.addedTemplate = true;
+    }
+
+    if (window.mainPage.project) {
+      this.$el.find('#closeOverlay').show();
+      window.mainPage.overlay.addCloseListeners();
+    } else {
+      this.$el.find('#closeOverlay').hide();
+      window.mainPage.overlay.removeCloseListeners();
+    }
+
+    if (window.mainPage.currentUser.isLoggedIn()) {
+      this.$el.find('#loginLinks').hide();
+      this.$el.find('#logoutLinks').show();
+    } else {
+      this.$el.find('#loginLinks').show();
+      this.$el.find('#logoutLinks').hide();
+    }
+  }
+});
+
+export default StartingScreen;
+*/
