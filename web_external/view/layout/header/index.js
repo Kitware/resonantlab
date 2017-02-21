95c4149a11c683d1520fe2c7f44cf9e98a5e7684
Roni Choudhury <roni.choudhury@kitware.com>
2017-02-21 17:58:32 -0500

    Set up proper behaviors for project name editing

--- web_external/view/layout/header/index.js
+++ web_external/view/layout/header/index.js
@@ -1,4 +1,5 @@
-import { select } from 'd3-selection';
+import { event,
+         select } from 'd3-selection';
 
 import { action } from '../../../redux/action';
 import { appMode } from '../../../redux/reducer';
@@ -23,23 +24,56 @@ const initialize = (sel) => {
   select('#aboutResLabHeaderButton').on('click', () => {
     store.dispatch(action.switchMode(appMode.startScreen));
   });
+
   select('#hamburgerButton').on('click', () => {
     console.log('click #hamburgerButton');
   });
+
   select('#helpButton').on('click', () => {
     console.log('click #helpButton');
   });
+
   select('#projectVisibilityButton').on('click', () => {
     console.log('click #projectVisibilityButton');
   });
-  select('#projectName').on('focus', () => {
-    console.log('focus #projectName');
-  });
+
   select('#projectName').on('blur', () => {
-    console.log('blur #projectName');
+    // Whenever the project name field blurs (that is, loses focus), we have to
+    // check whether we need to update the project name on the server.
+    //
+    // Grab the new name from the text field, and the original name from
+    // application state.
+    const textField = select('#projectName')
+    const newName = textField.text();
+    const oldName = store.getState().getIn(['project', 'name']);
+
+    if (newName === '') {
+      // If the user did something crazy like delete the name, then restore it
+      // to what it was before the editing began.
+      textField.text(oldName);
+    } else if (newName !== oldName) {
+      // Otherwise, as long as the name has changed, initiate a change to the
+      // project item's name as well.
+      console.log('TODO: change project name');
+    }
   });
+
   select('#projectName').on('keydown', () => {
-    console.log('keydown #projectName');
+    // We need to treat two keystrokes specially - the enter key normally
+    // inserts a linebreak in a content-editable span element, but we want it to
+    // "save" the filename. By contract, the escape key normally blurs the
+    // element, but we also want to "cancel" the editing operation first.
+    switch (event.keyCode) {
+    case 13: // Enter key
+      select('#projectName').node().blur();
+      event.preventDefault();
+      break;
+
+    case 27: // Escape key.
+      select('#projectName')
+        .text(store.getState().getIn(['project', 'name']));
+      break;
+    }
   });
 };
 
