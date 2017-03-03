941a6e58ee458e3bf5e2e314a1595885d5503a40
Roni Choudhury <roni.choudhury@kitware.com>
2017-03-03 17:38:32 -0500

    Abstract list of application modes into a yaml file

--- web_external/redux/appMode/index.js
+++ web_external/redux/appMode/index.js
@@ -1,11 +1,7 @@
 import { makeEnum } from '~reslab/util/makeEnum';
+import modes from './modes.yml';
 
-const appMode = makeEnum('appMode', [
-  'startScreen',
-  'loginDialog',
-  'openProjectDialog',
-  'project'
-]);
+const appMode = makeEnum('appMode', modes);
 
 export {
   appMode
