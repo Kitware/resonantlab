e20e9e1638b5bc0fbb76ba213ea949a45b0c3fe0
Roni Choudhury <roni.choudhury@kitware.com>
2017-03-03 14:27:32 -0500

    Separate clear and submit functions into class methods

--- web_external/view/overlay/LoginDialog/index.js
+++ web_external/view/overlay/LoginDialog/index.js
@@ -25,23 +25,36 @@ class LoginDialog {
     };
 
     this.el.select('a#close-login').on('click', () => {
-      clear();
+      this.clear();
       store.dispatch(action.lastMode());
     });
 
-    this.el.select('a#submit-login').on('click', () => {
-      const username = this.el.select('#g-login').property('value');
-      const password = this.el.select('#g-password').property('value');
+    // NOTE: need to use an arrow function for the callback to preserve "this"
+    // context.
+    this.el.select('a#submit-login').on('click', () => this.submit());
+  }
 
-      login(username, password).then(userInformation, xhr =>
-        this.el.select('.g-validation-failed-message')
-        .text(xhr.responseJSON.message)
-      ).then(info => {
-        clear();
-        store.dispatch(action.login(info.login, info.private, info.public));
-        store.dispatch(action.lastMode());
-      });
+  submit () {
+    const username = this.el.select('#g-login').property('value');
+    const password = this.el.select('#g-password').property('value');
+
+    login(username, password).then(userInformation, xhr =>
+      this.el.select('.g-validation-failed-message')
+      .text(xhr.responseJSON.message)
+    ).then(info => {
+      this.clear();
+      store.dispatch(action.login(info.login, info.private, info.public));
+      store.dispatch(action.lastMode());
     });
+  }
+
+  clear () {
+    this.el.select('#g-login')
+      .property('value', '');
+    this.el.select('#g-password')
+      .property('value', '');
+    this.el.select('.g-validation-failed-message')
+      .text('');
   }
 }
 
